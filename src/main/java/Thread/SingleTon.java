396d1d8cb3af1504905d4879cf574afc5b1f80d5
LinZiYu1996 <2669093302@qq.com>
2019-04-13 16:39:39 +0800

    first

---
+++ src/main/java/Thread/SingleTon.java
@@ -0,0 +1,33 @@
+package Thread;
+
+
+/*
+*
+ * @Author Mr.Lin
+ * @Description //线程安全的单例模式
+ *
+ * @Date 16:36 2019/4/13
+ **/
+public class SingleTon {
+
+    //使用volatile是为了JVM的指令重排
+    private volatile static SingleTon uniqueInstance;
+
+    private SingleTon(){
+
+    }
+
+    public static SingleTon getUniqueInstance(){
+        //先判断对象是否已经实例过，没有的话才进入加锁代码
+        if (uniqueInstance == null) {
+            synchronized (SingleTon.class){
+                if (uniqueInstance == null){
+                    uniqueInstance = new SingleTon();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+
+
+}
