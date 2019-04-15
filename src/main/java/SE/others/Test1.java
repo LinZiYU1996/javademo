60ebd914f7a051492ac71f823638f95cb9f0de85
LinZiYu1996 <2669093302@qq.com>
2019-04-15 21:54:16 +0800

    first

---
+++ src/main/java/SE/others/Test1.java
@@ -0,0 +1,17 @@
+package SE.others;
+
+
+/*
+*
+ * @Author Mr.Lin
+ * @Description //TODO
+ *
+ * 测试 >>  运算符
+ * @Date 21:51 2019/4/15
+ **/
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println(4 >> 2);//4 对应二进制 100  >> 右移运算符  右移两位  变成  001 也就是 1
+    }
+}
