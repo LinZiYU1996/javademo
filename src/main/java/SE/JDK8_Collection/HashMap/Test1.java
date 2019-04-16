e0ce9d8c500f7bcd0c1358eb9b87c8676646658b
LinZiYu1996 <2669093302@qq.com>
2019-04-16 20:22:57 +0800

    first

---
+++ src/main/java/SE/JDK8_Collection/HashMap/Test1.java
@@ -0,0 +1,22 @@
+package SE.JDK8_Collection.HashMap;
+
+
+import java.util.HashMap;
+
+/*
+*
+ * @Author Mr.Lin
+ * @Description //TODO
+ *
+ *
+ * @Date 11:00 2019/4/16
+ **/
+public class Test1 {
+
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+
+    }
+
+}
